13a3a3c0653b665222231cb38107b311a1d7a3af
nicmackenzie <nicmackenzie@live.com>
2023-07-25 07:57:39 +0300

    change error message

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -18,8 +18,28 @@ class ApplicationController < Sinatra::Base
     response.headers["Access-Control-Allow-Origin"] = "*"
     200
   end
+
+  # helper method to return json data
+  def create_response(success, status_code, message, data)
+    if !success
+      status status_code
+      response = { success: false, message: message, data: nil }
+    else
+      response = { success: true, message: nil, data: data }
+    end
+    return response
+  end
   
-  
+  post '/login' do
+    user = User.find_by(email: params[:email])
+    if user == nil
+      response = create_response(false, 401, 'Invalid email or password', nil)
+      return response.to_json
+    else
+      response = create_response(true, 200, nil , user)
+      response.to_json(include: :userstacks)
+    end
+  end
 end
  
   
