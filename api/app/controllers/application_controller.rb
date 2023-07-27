ee0d796de9f6af260434c6cc4615c6ff03ff8595
nicmackenzie <nicmackenzie@live.com>
2023-07-27 08:43:17 +0300

    configure cors and json parser

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,22 +2,22 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  use Rack::JSONBodyParser
+  # use Rack::JSONBodyParser
 
-  configure do
-    enable :cross_origin
-  end
+  # configure do
+  #   enable :cross_origin
+  # end
   
-  before do
-    response.headers['Access-Control-Allow-Origin'] = '*'
-  end
+  # before do
+  #   response.headers['Access-Control-Allow-Origin'] = '*'
+  # end
   
-  options "*" do
-    response.headers["Allow"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    200
-  end
+  # options "*" do
+  #   response.headers["Allow"] = "GET, POST, PUT, DELETE, OPTIONS"
+  #   response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  #   response.headers["Access-Control-Allow-Origin"] = "*"
+  #   200
+  # end
 
   # helper method to return json data
   def create_response(success, status_code, message, data)
@@ -37,9 +37,81 @@ class ApplicationController < Sinatra::Base
       return response.to_json
     else
       response = create_response(true, 200, nil , user)
-      response.to_json(include: :userstacks)
+      response.to_json
     end
   end
+
+  post '/signup' do
+    user = find_by(email: params[:email])
+
+    if user
+      response = create_response(false, 401, 'Email already exists', nil)
+    else
+       User.create(user_name: params[:name], email: params[:email],devtype: params[:devtype],
+                   experience: params[:experience],image_url: '')
+    end
+
+  end
+
+  get '/stacks' do
+    stacks = Stack.all
+    stacks.to_json
+    response = create_response(true,200,nil,stacks)
+    response.to_json
+  end
+
+  get '/stacks/user/:id' do
+    stacks = Userstack.where(user_id: params[:id])
+    response = create_response(true, 200, nil , stacks)
+    response.to_json
+  end
+
+  patch '/users/edit/:id' do
+    # puts params
+    id = params[:id]
+    user = User.find(id)
+    # user_stacks = Userstack.find_by(user_id: id)
+    ActiveRecord::Base.transaction do
+      user.update(
+        user_name: params[:name],
+        email: params[:email],
+        devtype: params[:devtype],
+        experience: params[:experience]
+      )
+
+      user.userstacks.destroy_all
+    
+      params[:stacks].each do |stack|
+        Userstack.create(user_id: id, stack_id: stack) 
+      end
+    end
+    
+    response = create_response(true, 200, nil , user)
+    response.to_json
+  end
+
+  post '/users/updatelinks/:id' do
+    id = params[:id]
+    data = JSON.parse(request.body.read)
+    user = User.find(id)
+
+    # puts data
+
+    ActiveRecord::Base.transaction do
+  
+      user.userlinks.destroy_all
+
+      data["links"].each do |link_data|
+        Userlink.create(name: link_data["platform"], user_id: id, link_url: link_data["link"])
+      end
+      
+      response = create_response(true, 201, 'Updated successfully', nil)
+      response.to_json
+
+    end
+
+  end
+
 end
  
   
