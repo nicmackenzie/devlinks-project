0fbc85d5e8a6a644c71067a272eb043d2938757a
nicmackenzie <nicmackenzie@live.com>
2023-07-27 08:43:56 +0300

    change experience values

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,9 +1,9 @@
 puts "👨‍🦱 Seeding user data..."
 
 User.create(user_name: 'Nick Mackenzie', email: 'nicmackenzie@live.com',devtype: 'full stack',experience: 4,image_url: 'https://igqjyslipxclzwcefxrb.supabase.co/storage/v1/object/public/avatars/nick_mackenzie.jpg')
-User.create(user_name: 'Angela Yu', email: 'angela@techbrew.io',devtype: 'full stack',experience: 15,image_url: 'https://igqjyslipxclzwcefxrb.supabase.co/storage/v1/object/public/avatars/angela_yu.jpg')
-User.create(user_name: 'Brad Pitt', email: 'pitt@brajelina.org',devtype: 'frontend',experience: 5,image_url: 'https://igqjyslipxclzwcefxrb.supabase.co/storage/v1/object/public/avatars/brad_pitt.jpg')
-User.create(user_name: 'Angelina Jolie', email: 'angelina@brajelina.org',devtype: 'backend',experience: 8,image_url: 'https://igqjyslipxclzwcefxrb.supabase.co/storage/v1/object/public/avatars/jelina_jolie.jpg')
+User.create(user_name: 'Angela Yu', email: 'angela@techbrew.io',devtype: 'full stack',experience: 2,image_url: 'https://igqjyslipxclzwcefxrb.supabase.co/storage/v1/object/public/avatars/angela_yu.jpg')
+User.create(user_name: 'Brad Pitt', email: 'pitt@brajelina.org',devtype: 'frontend',experience: 3,image_url: 'https://igqjyslipxclzwcefxrb.supabase.co/storage/v1/object/public/avatars/brad_pitt.jpg')
+User.create(user_name: 'Angelina Jolie', email: 'angelina@brajelina.org',devtype: 'backend',experience: 1,image_url: 'https://igqjyslipxclzwcefxrb.supabase.co/storage/v1/object/public/avatars/jelina_jolie.jpg')
 
 puts "📳 Seeding stacks data..."
 
